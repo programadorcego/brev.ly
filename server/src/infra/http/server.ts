de9d0b0a32a1e49821d435a0e41a1fdd65e228dc
Willian Pereira <will.sp365@gmail.com>
2026-03-13 15:52:46 -0300

    Servidor HTTP com fastify

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,8 +1,27 @@
 import fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
 import { env } from "@/env";
+import { hasZodFastifySchemaValidationErrors, serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 
 const server = fastify();
+
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
+server.setErrorHandler((error, request, reply) => {
+    if (hasZodFastifySchemaValidationErrors(error)) {
+        return reply.status(400).send({
+            message: "Validation Error",
+            issues: error.validation,
+        });
+    }
+
+    console.error(error);
+
+    return reply.status(500).send({
+        message: "Internal Server Error",
+    });
+});
 
 server.register(fastifyCors, {
     origin: "*",
