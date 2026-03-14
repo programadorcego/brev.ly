b63850c0157ce088533a284c328b74a508a53be7
Willian Pereira <will.sp365@gmail.com>
2026-03-13 21:40:16 -0300

    Estratégias de IDs únicos

--- server/src/infra/db/schemas/links.ts
+++ server/src/infra/db/schemas/links.ts
@@ -1,10 +1,10 @@
 import { bigint, pgTable, text, timestamp } from "drizzle-orm/pg-core";
-import { randomUUID } from "node:crypto";
+import { uuidv7 } from "uuidv7";
 
 export const links = pgTable("links", {
     id: text("id")
         .primaryKey()
-        .$defaultFn(() => randomUUID()),
+        .$defaultFn(() => uuidv7()),
     originalLink: text("original_link").notNull(),
     shortLink: text("short_link").notNull().unique(),
     hits: bigint("hits", { mode: "number" }).default(0),
--- server/src/infra/http/routes/create-link.ts
+++ server/src/infra/http/routes/create-link.ts
@@ -1,3 +1,5 @@
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 
@@ -9,7 +11,7 @@ export const createLinkRoute: FastifyPluginAsyncZod = async server => {
                 summary: "Create a short link",
                 body: z.object({
                     originalLink: z.string().url(),
-                    shortLink: z.string(),
+                    shortLink: z.string().regex(/^[a-zA-Z0-9_\-]+$/),
                 }),
                 response: {
                     201: z.object({ linkId: z.string() }),
@@ -21,6 +23,10 @@ export const createLinkRoute: FastifyPluginAsyncZod = async server => {
         },
 
         async (request, reply) => {
+            await db.insert(schema.links).values({
+                originalLink: "https://google.com",
+                shortLink: "abc123",
+            });
             return reply.status(201).send({ linkId: "123" })
         }
     );
