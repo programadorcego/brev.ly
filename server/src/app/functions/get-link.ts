e859e82c4dc68e93913da7aa8cd5855600889d87
Willian Pereira <will.sp365@gmail.com>
2026-03-15 12:02:11 -0300

    Função para obter o link original

---
+++ server/src/app/functions/get-link.ts
@@ -0,0 +1,42 @@
+import { z } from "zod/mini";
+import { ResourceNotFoundError } from "./errors/resource-not-found";
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
+import { eq } from "drizzle-orm";
+import { Either, makeLeft, makeRight } from "@/infra/shared/either";
+
+const getLinkSchema = z.object({
+    shortLink: z.string(),
+});
+
+type GetLinkSchema = z.input<typeof getLinkSchema>;
+
+type GetLinkResponse = {
+    id: string,
+    originalLink: string,
+    shortLink: string,
+    hits: number | null,
+    createdAt: Date,
+};
+
+export async function getLink(data: GetLinkSchema): Promise<Either<ResourceNotFoundError, GetLinkResponse>> {
+    const { shortLink } = data;
+
+    const link = (await db
+        .select({
+            id: schema.links.id,
+            originalLink: schema.links.originalLink,
+            shortLink: schema.links.shortLink,
+            hits: schema.links.hits,
+            createdAt: schema.links.createdAt,
+        })
+        .from(schema.links)
+        .where(eq(schema.links.shortLink, shortLink))
+        .limit(1))[0];
+
+    if (!link) {
+        return makeLeft(new ResourceNotFoundError());
+    }
+
+    return makeRight(link);
+}
